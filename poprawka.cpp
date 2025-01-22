fab3f5cd9e39ef5da052d7de51cd49e470d4f2ce
BorysBorkowski <borysbork2009@gmail.com>
2025-01-22 18:05:55 +0100

    Add files via upload

---
+++ poprawka.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int tablica[3][5] = {};
+    for(int i=0; i<=2; i++)
+    {
+        for(int j=0; j<=4; j++)
+        {
+        cout<<tablica[i][j];
+        }
+        cout<<endl;
+    }
+    return 0;
+}
